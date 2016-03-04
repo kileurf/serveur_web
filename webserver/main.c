5b022c27ca5860fcfcaaf24293cd01d37d779f37
Alexis Bulteel <alexisbu@hotmail.fr>
2016-03-04 11:27:42 +0100

    fork marche

--- webserver/main.c
+++ webserver/main.c
@@ -11,24 +11,28 @@
 int main(void){
 	int socket_serveur=creer_serveur(8080);
 	initialiser_signaux();
-	char *buf = malloc(sizeof(char)*80);
+	
 	int socket_client;
 	while (1) {
 		socket_client = accept ( socket_serveur , NULL , NULL );
 		if ( socket_client == -1){
 			perror ( " accept " ); /* traitement d ’ erreur */
 		}
-		const char * message_bienvenue = "La sortie c'est sur la petite croix (rouge) merci d'appuyer 			dessus.	\n Nutella et Caramel\n J'vous aimes putain";
-		write ( socket_client , message_bienvenue , strlen(message_bienvenue));
+		const char * message_bienvenue = "La sortie c'est sur la petite croix (rouge) merci 			d'appuyer dessus.\n Nutella et Caramel\n J'vous aimes putain";
+		int pid;
+		if((pid=fork())==0){
+			char *buf = malloc(sizeof(char)*80);
+			write ( socket_client , message_bienvenue , strlen(message_bienvenue));
 
-	FILE *f = fdopen(socket_client, "r+");
- 	do {fgets(buf, sizeof(buf),f);
- 	printf("%s",buf);
- 	fflush(stdout);
- 	write ( socket_client, buf , strlen (buf));
-	}while(buf[0]!='.' && buf[1]!='q');
- 	free(buf);
-	close(socket_client);
+			FILE *f = fdopen(socket_client, "r+");
+ 			do {fgets(buf, sizeof(buf),f);
+ 				printf("%s",buf);
+ 				fflush(stdout);
+ 				write ( socket_client, buf , strlen (buf));
+			}while(buf[0]!='.' && buf[1]!='q');
+ 			free(buf);
+			close(socket_client);
+		}
 	}
 	
 	return 0;
