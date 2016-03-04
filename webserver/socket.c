b1f17ba6e26c5ec79f9a21d8ce446dddbb51eba9
Alexis Bulteel <alexisbu@hotmail.fr>
2016-03-04 11:02:36 +0100

    addr

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,6 +7,9 @@ int socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
 		perror ( " socket_serveur " );
 		/* traitement de l ’ erreur */
 	}
+int optval = 1;
+if ( setsockopt ( socket_serveur , SOL_SOCKET , SO_REUSEADDR , & optval , sizeof ( int )) == -1)
+		perror ( " Can not set SO_REUSEADDR option " );
 struct sockaddr_in saddr ;
 	saddr.sin_family = AF_INET ; /* Socket ipv4 */
 	saddr.sin_port = htons (port); /* Port d ’ écoute */
